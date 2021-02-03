295eb3776d923dcbad18126e9898e077a3164c7b
Leandro Inocencio <cesio.arg@gmail.com>
2021-02-03 10:36:23 -0500

    Files modified for build oiio with python 2.x

--- ports/pybind11/portfile.cmake
+++ ports/pybind11/portfile.cmake
@@ -6,24 +6,19 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-vcpkg_find_acquire_program(PYTHON3)
-get_filename_component(PYPATH ${PYTHON3} PATH)
-vcpkg_add_to_path("${PYPATH}")
+vcpkg_find_acquire_program(PYTHON2)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
         -DPYBIND11_TEST=OFF
-        -DPYTHONLIBS_FOUND=ON
-        -DPYTHON_INCLUDE_DIRS=${CURRENT_INSTALLED_DIR}/include
-        -DPYTHON_MODULE_EXTENSION=.dll
+        -DPYBIND11_FINDPYTHON=ON
+        -DPYTHON_EXECUTABLE=${PYTHON2}
     OPTIONS_RELEASE
         -DPYTHON_IS_DEBUG=OFF
-        -DPYTHON_LIBRARIES=${CURRENT_INSTALLED_DIR}/lib/python39.lib
     OPTIONS_DEBUG
         -DPYTHON_IS_DEBUG=ON
-        -DPYTHON_LIBRARIES=${CURRENT_INSTALLED_DIR}/debug/lib/python39_d.lib
 )
 
 vcpkg_install_cmake()
